809e40a477f0b97e420b9d0de1e0e098580e4969
raafaa-1215 <131355317+raafaa-1215@users.noreply.github.com>
2024-06-07 09:23:01 +0100

    start fase 2

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
-const express = require("express");
-const port = 4000;
-const bodyParser = require("body-parser");
+// node imports
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+// script imports
+const handler = require('./scripts/request-handler.js');
+
 const app = express();
-const handler = require("scripts/request-handler.js");
+const port = 4000;
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded());
+app.use(express.static(path.join(__dirname, 'styles')));
+app.use(express.static(path.join(__dirname, 'images')));
+app.use(express.static(path.join(__dirname, 'scripts')));
 
 app.get("/", handler.getIndex);
+var userData = {"username":"Rafa", "password":"teste123"};
+app.get("/profile", (req, res) => {handler.getProfile(req,res, userData);});
+var incomeStringsData = [];
+app.get("/incomeStrings", (req, res) => {handler.getIncomeStrings(req,res, incomeStringsData);});
 
 app.listen(port, function () {
     console.log(`app running at http://localhost:${port}`);
--- scripts/request-handler.js
+++ scripts/request-handler.js
@@ -2,6 +2,14 @@ function getIndex(req, res) {
     res.render('index');
 }
 
+function getProfile(req, res, userData) {
+    res.render('profile', { userData });
+}
 
+function getIncomeStrings(req, res, incomeStringsData) {
+    res.render('income-strings', { incomeStringsData });
+}
 
 module.exports.getIndex = getIndex;
+module.exports.getProfile = getProfile;
+module.exports.getIncomeStrings = getIncomeStrings;
